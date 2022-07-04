91c0f3f0c001f9089302e445ef4e88beb0210560
Zeeshan Qasim <zqasim132@gmail.com>
2022-07-04 12:35:33 +0500

    Implemented the google map Signed-off-by: Zeeshan Qasim <zqasim132@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,8 +9,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCDpfZsdAV2xTCjEieMf4bnBJlKP5EI4zY',
+      libraries: ['places']
+    })
   ],
+  // https://maps.googleapis.com/maps/api/js?key=AIzaSyCDpfZsdAV2xTCjEieMf4bnBJlKP5EI4zY&libraries=places
   providers: [],
   bootstrap: [AppComponent]
 })
